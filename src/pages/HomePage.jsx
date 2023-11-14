34a51466c55057614e7118a4b707216e41072b05
unknown <tifalrizkyarjuna@gmail.com>
2023-11-14 15:37:43 +0700

    updates

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,30 +1,66 @@
-import { Container, Row, Col } from "react-bootstrap"
+import { Container, Row, Col, Button } from "react-bootstrap"
+
 import Banner from '../assets/images/banner.jpg'
 import backgroundImage from '../assets/images/background.png'
 
+import { productTerbaru } from "../data"
+
+import { useNavigate } from "react-router-dom"
+import { Card } from "primereact/card"
+
 const HomePage = () => {
+  let navigate = useNavigate();
+  
   return (
     <div className="homepage">
       <header className="w-100 min-vh-100 d-flex align-items-center">
         <Container>
           <Row className="header-box d-flex align-items-center">
-            <Col>
-              <h1>Choose Your <br /> <span>Vapes Category</span> <br /> Here !</h1>
-              <p>The best vape store this year with more than<br />five thousand trusted customers.</p>
-              <button className="btn btn-outline-danger">Explore Now!</button>
+            <Col lg="6">
+              <h1 className="mb-4">Choose Your <br /> <span>Vapes Category</span> <br /> Here !</h1>
+              <p className="mb-4">The best vape store this year with more than<br />five thousand trusted customers.</p>
+              <button className="btn btn-outline-danger mb-xs-0 mb-2">Explore Now!</button>
             </Col>
-            <Col className="text-center">
+            <Col lg="6" className="text-center pt-lg-0 pt-5">
               <img src={Banner} alt="banner-img" />
             </Col>
           </Row>
         </Container>
       </header>
-      <div className="w-100 min-vh-100">
+      <div className="product w-100 min-vh-100">
         <Container>
           <Row>
             <Col>
-              <h1>Kelas Terbaru</h1>
-              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore delectus exercitationem veritatis aperiam sequi consectetur, animi qui possimus blanditiis provident ipsam facere corporis consequatur ipsa dignissimos placeat quo minima quidem.</p>
+              <h1 className="text-center fw-bold">Product Terbaru</h1>
+              <p className="text-center">Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </Col>
+          </Row>
+          <Row>
+            {productTerbaru.map((product) => {
+              return (
+              <Card key={product.id}>
+                <img src={product.image} className="mb-3 rounded-top"/>
+                <div className="star mb-2">
+                  <i className={product.star1}></i>
+                  <i className={product.star2}></i>
+                  <i className={product.star3}></i>
+                  <i className={product.star4}></i>
+                  <i className={product.star5}></i>
+                </div>
+                <h5 className="mb-2">{product.title}</h5>
+                <div className="ket d-flex justify-content-between align-items-center mt-3">
+                  <p className="m-0 text-danger fw-bold">{product.price}</p>
+                  <button className="btn btn-danger rounded-1">{product.buy}</button>
+                </div>
+              </Card>
+              );
+            })}
+          </Row>
+          <Row>
+            <Col className="text-center">
+              <Button className="btn btn-success rounded-5 btn-lg" onClick={() => navigate('/product')}>Lihat Semua Product
+              <i className="fa-solid fa-chevron-right ms-2"></i>
+              </Button>
             </Col>
           </Row>
         </Container>
